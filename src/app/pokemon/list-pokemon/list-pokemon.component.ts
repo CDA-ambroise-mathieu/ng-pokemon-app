d8a5b3c80e37fb5d2c9c9b43d864c98dc7e2a5a4
Mathieu <mat.ambroise@gmail.com>
2022-10-14 12:33:56 +0200

    mise en place des services

--- src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { POKEMONS } from '../mock-pokemon-list';
 import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-list-pokemon',
   templateUrl: 'list-pokemon.component.html',
   styles: [],
 })
-export class ListPokemonComponent {
-  pokemonList: Pokemon[] = POKEMONS;
+export class ListPokemonComponent implements OnInit {
+  pokemonList: Pokemon[];
 
-  constructor(private router: Router){}
+  constructor(private router: Router, private pokemonService: PokemonService) {}
 
-  goToPokemon(pokemon: Pokemon){
-    this.router.navigate(['/pokemon', pokemon.id])
+  ngOnInit(): void {
+    this.pokemonList = this.pokemonService.getPokemonList();
+  }
+
+  goToPokemon(pokemon: Pokemon) {
+    this.router.navigate(['/pokemon', pokemon.id]);
   }
 }
